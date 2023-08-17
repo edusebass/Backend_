35c778399d7c47fc7c3c6c120fae6bcadcde044e
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-08-16 20:37:08 -0500

    Express Server Creation

---
+++ 3.1 Express Server/index.js
@@ -0,0 +1,22 @@
+//Create an Express Server
+/*
+1. Create a directory
+2. Create index.js
+3. Initialise npm (//Step for create express 
+                    npm init -y
+4. Install the Express package
+(npm i express)
+5. Write Ser application in index.js (code this down) 
+6. Start server
+*/
+
+import express from "express";
+
+//This is a app that obligatory
+const app = express();
+const port = 3000;
+
+//3000 is the port of the server
+app.listen(port, () =>{
+    console.log(`Server running on port 3000 ${port}.`); //this is callback function
+});
