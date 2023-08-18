4ce3b94a6e7c080ad8a7f12ce1f9bd0b3f39fe35
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-08-17 22:33:50 -0500

    3.2 COMPLETE

--- 3.2 HTTP Requests/index.js
+++ 3.2 HTTP Requests/index.js
@@ -1,7 +1,24 @@
+//diferentes tipos de solicitudes (GET, POST, PUT, DELETE, etc.)
+//request /
+
 import express from 'express';
 
 const app = express();
 const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Hello</h1>`);
+});
+
+app.get('/contact', (req, res) => {
+    res.send(`<h1>Contact me</h1>
+    <p>Telefono: 0986572316</p>`);
+});
+
+app.get('/about', (req, res) => {
+    res.send(`<h1>About me</h1>
+    <p>I am Eduardo Almachi</p>`);
+});
 
 app.listen(port, () => {
     console.log(`Listen port ${port}`);
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This respository contains about the backend from the web
 ---
 
 - 3.1 Express Server: Contain about how create a express server
+- 3.2 HTTP Requests: Contain about how create a http server
