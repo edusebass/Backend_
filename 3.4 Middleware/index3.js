e529aff78ec10197ae0ed918fa15bab66349660c
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-08-19 14:56:07 -0500

    update about 3.4

--- 3.4 Middleware/index3.js
+++ 3.4 Middleware/index3.js
@@ -1,7 +1,21 @@
+/*
+1. Run index3.js with nodemon
+2. Create ypur own function called logger (req, res, next)
+3. Inside your function console.log() the request method and the request URL
+4. Rememeber to call next()
+5. Mount  yout middleware function using the Express .use() method
+6. Test logging on local host and with Postman
+*/
 import express from "express";
 
 const app = express();
 const port = 3000;
+
+function logger(req, res, next) {
+  console.log(req.method);
+  console.log(req.url);
+  next();
+}
 
 app.use(logger);
 
